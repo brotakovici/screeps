ff71062b8892cf3c9efb295ba2bbeee7e51438d1
brotakovici <brotakovici@gmail.com>
2016-11-23 12:13:09 +0000

    Added gitignore

--- main.js
+++ main.js
@@ -11,35 +11,35 @@ module.exports.loop = function () {
             console.log('Clearing non-existing creep memory:', name);
          }
     }
-    
+
     // Creep generation
     // code here
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     //console.log('Harvesters: ' + harvesters.length);
     //console.log('Upgraders: ' + upgraders.length);
-    
+
     //console.log('Energy: ' + Room.energyAvailable);
-    
+
     if(harvesters.length < 2)
     {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
     }
-    
+
     if(upgraders.length < 1)
     {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
         console.log('Spawning new upgrader: ' + newName);
     }
-    
+
     // Send everyone to work
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
         }
-        
+
         if(creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
         }
@@ -48,5 +48,6 @@ module.exports.loop = function () {
             roleBuilder.run(creep);
         }
         */
+
     }
 }
